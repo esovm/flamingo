961318cc01d5957d5a3eb9c0ffc107f189b4551d
Tomer Shechner <tomers1504@gmail.com>
2020-05-03 02:51:12 +0300

    Improve library loading

--- src/main.c
+++ src/main.c
@@ -1,4 +1,9 @@
+#include <string.h>
 #include <setjmp.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <pwd.h>
+#include <editline/readline.h>
 
 #include "fl_lib.h"
 #include "flamingo.h"
@@ -13,19 +18,40 @@ static void p_error(Fl_Context *ctx, const char *message, Fl_Object *call_list) 
     longjmp(global_execution_context, -1);
 }
 
+static void p_load(Fl_Context *ctx, const char *fn) {
+    FILE *fp;
+    if ((fp = fopen(fn, "r"))) {
+        Fl_run_file(ctx, fp);
+    } else {
+        const char *homedir = getenv("HOME");
+        /* 16 is strlen of "/.flamingo/lib/" */
+        char *full_path = malloc(strlen(homedir) + strlen(fn) + 16);
+        if (!homedir) /* HOME is not set */
+            homedir = getpwuid(getuid())->pw_dir;
+        if (!full_path) {
+            fputs("malloc failure...\n", stderr);
+            exit(1);
+        }
+        strcpy(full_path, homedir);
+        strcat(full_path, "/.flamingo/lib/");
+        strcat(full_path, fn);
+        if (!(fp = fopen(full_path, "r")))
+            Fl_error(ctx, "Could not load library");
+        free(full_path);
+        Fl_run_file(ctx, fp);
+    }
+}
+
 int main(int argc, char **argv) {
     Fl_Object *obj;
-    FILE *base, *volatile fp = stdin;
+    FILE *volatile fp = stdin;
     Fl_Context *ctx = Fl_open(buf, sizeof(buf));
 
-    if (!(base = fopen("lib/base.fl", "r")))
-        Fl_error(ctx, "Could not load base library");
-    Fl_run_file(ctx, base);
-
+    p_load(ctx, "base.fl");
     aux_register_all(ctx);
 
     if (argc > 1 && !(fp = fopen(argv[1], "r")))
-        Fl_error(ctx, "Could not open file");
+        Fl_error(ctx, "could not open file");
     if (fp == stdin) {
         Fl_handlers(ctx)->error = p_error;
         printf("Flamingo %s\n", FLAMINGO_VERSION);
