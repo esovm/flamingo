0b3859460ef27bdab3a08c18b926992cddfed260
Tomer Shechner <tomers1504@gmail.com>
2020-05-08 02:39:15 +0300

    Add -s option (execute string), doesn't work atm

--- src/main.c
+++ src/main.c
@@ -36,7 +36,7 @@ static void p_load(Fl_Context *ctx, const char *fn) {
         strcat(full_path, "/.flamingo/lib/");
         strcat(full_path, fn);
         if (!(fp = fopen(full_path, "r")))
-            Fl_error(ctx, "Could not load library");
+            Fl_error(ctx, "could not load library");
         free(full_path);
         Fl_run_file(ctx, fp);
     }
@@ -46,12 +46,37 @@ int main(int argc, char **argv) {
     Fl_Object *obj;
     FILE *volatile fp = stdin;
     Fl_Context *ctx = Fl_open(buf, sizeof(buf));
+    char *exec_str = NULL;
+    int c;
 
     p_load(ctx, "base.fl");
     bs_register_all(ctx);
 
-    if (argc > 1 && !(fp = fopen(argv[1], "r")))
+    while ((c = getopt(argc, argv, "s:")) != -1) {
+        switch (c) {
+        case 's':
+            exec_str = optarg;
+            break;
+        case '?':
+            if (optopt == 's')
+                fprintf(stderr, "Option -%c requires an argument\n", optopt);
+            else
+                fprintf(stderr, "Unknown option -%c\n", optopt);
+            /* fallthrough */
+        default:
+            return 1;
+        }
+    }
+    if (exec_str) {
+        if (!(fp = tmpfile())) {
+            fputs("unexpected error, could not execute given string\n", stderr);
+            return 1;
+        }
+        fputs(exec_str, fp);
+    } else if (argc > 1 && !(fp = fopen(argv[1], "r"))) {
         Fl_error(ctx, "could not open file");
+    }
+
     if (fp == stdin) {
         Fl_handlers(ctx)->error = p_error;
         printf("Flamingo %s on %s\n", FLAMINGO_VERSION, os_name());
@@ -66,7 +91,7 @@ int main(int argc, char **argv) {
         if (!(obj = Fl_readfp(ctx, fp)))
             break;
         obj = Fl_eval(ctx, obj);
-        if (fp == stdin) {
+        if (fp == stdin || exec_str) {
             Fl_writefp(ctx, obj, stdout);
             putchar('\n');
         }
