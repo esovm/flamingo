de8b102fa79f3c0659622afad57a104352c7ae68
Tomer Shechner <tomers1504@gmail.com>
2020-05-12 15:51:00 +0300

    Add configuration file

---
+++ include/config.h
@@ -0,0 +1,13 @@
+/* Flamingo configuration file - edit me as you wish */
+
+#ifndef FLAMINGO_CONFIG_H
+#define FLAMINGO_CONFIG_H
+
+#include "util.h"
+
+#define FL_PROGRAM_NAME "flamingo"
+extern const char *program_name;
+
+#define FL_PATH "/." FL_PROGRAM_NAME
+
+#endif /* FLAMINGO_CONFIG_H */
